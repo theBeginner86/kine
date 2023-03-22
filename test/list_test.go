7a71eaec333a7b23b3b1ff127e06d055a45b2f12
Sachin Kumar Singh <sachin.singh@canonical.com>
2023-03-22 22:19:07 +0530

    More assertions

--- test/list_test.go
+++ test/list_test.go
@@ -18,7 +18,7 @@ func TestList(t *testing.T) {
 		g := NewWithT(t)
 
 		// Create some keys
-		keys := []string{"key/1", "key/2", "key/3"}
+		keys := []string{"/key/1", "/key/2", "/key/3"}
 		for _, key := range keys {
 			resp, err := client.Txn(ctx).
 				If(clientv3.Compare(clientv3.ModRevision(key), "=", 0)).
@@ -27,30 +27,73 @@ func TestList(t *testing.T) {
 
 			g.Expect(err).To(BeNil())
 			g.Expect(resp.Succeeded).To(BeTrue())
+			g.Expect(resp.Header.Revision).ToNot(BeZero())
 		}
 
 		t.Run("ListAll", func(t *testing.T) {
 			// Get a list of all the keys
-			resp, err := client.Get(ctx, "", clientv3.WithPrefix())
-
-			g.Expect(err).To(BeNil())
-			g.Expect(resp.Kvs).To(HaveLen(0))
-		})
-
-		t.Run("ListPrefix", func(t *testing.T) {
-			// Get a list of all the keys sice they have same prefix
-			resp, err := client.Get(ctx, "key", clientv3.WithPrefix())
+			resp, err := client.Get(ctx, "/key", clientv3.WithPrefix())
 
 			g.Expect(err).To(BeNil())
 			g.Expect(resp.Kvs).To(HaveLen(3))
+			g.Expect(resp.Header.Revision).ToNot(BeZero())
+			g.Expect(resp.Kvs[0].Key).To(Equal([]byte("/key/1")))
+			g.Expect(resp.Kvs[1].Key).To(Equal([]byte("/key/2")))
+			g.Expect(resp.Kvs[2].Key).To(Equal([]byte("/key/3")))
+
+		})
+
+		t.Run("ListPrefix", func(t *testing.T) {
+			// Create some keys
+			keys := []string{"key/sub/1", "key/sub/2", "key/other/1"}
+			for _, key := range keys {
+				resp, err := client.Txn(ctx).
+					If(clientv3.Compare(clientv3.ModRevision(key), "=", 0)).
+					Then(clientv3.OpPut(key, "value")).
+					Commit()
+
+				g.Expect(err).To(BeNil())
+				g.Expect(resp.Succeeded).To(BeTrue())
+				g.Expect(resp.Header.Revision).ToNot(BeZero())
+			}
+
+			// Get a list of all the keys sice they have '/key' prefix
+			resp, err := client.Get(ctx, "/key", clientv3.WithPrefix())
+
+			g.Expect(err).To(BeNil())
+			g.Expect(resp.Kvs).To(HaveLen(3))
+			g.Expect(resp.Header.Revision).ToNot(BeZero())
+			g.Expect(resp.Kvs[0].Key).To(Equal([]byte("/key/1")))
+			g.Expect(resp.Kvs[1].Key).To(Equal([]byte("/key/2")))
+			g.Expect(resp.Kvs[2].Key).To(Equal([]byte("/key/3")))
+
+			// Get a list of all the keys sice they have '/key/sub' prefix
+			resp, err = client.Get(ctx, "key/sub", clientv3.WithPrefix())
+
+			g.Expect(err).To(BeNil())
+			g.Expect(resp.Kvs).To(HaveLen(2))
+			g.Expect(resp.Header.Revision).ToNot(BeZero())
+			g.Expect(resp.Kvs[0].Key).To(Equal([]byte("key/sub/1")))
+			g.Expect(resp.Kvs[1].Key).To(Equal([]byte("key/sub/2")))
+
+			// Get a list of all the keys sice they have '/key/other' prefix
+			resp, err = client.Get(ctx, "key/other", clientv3.WithPrefix())
+
+			g.Expect(err).To(BeNil())
+			g.Expect(resp.Kvs).To(HaveLen(1))
+			g.Expect(resp.Header.Revision).ToNot(BeZero())
+			g.Expect(resp.Kvs[0].Key).To(Equal([]byte("key/other/1")))
+
 		})
 
 		t.Run("ListRange", func(t *testing.T) {
 			// Get a list of with key/1, as only key/1 falls within the specified range.
-			resp, err := client.Get(ctx, "key/1", clientv3.WithRange(""))
+			resp, err := client.Get(ctx, "/key/1", clientv3.WithRange(""))
 
 			g.Expect(err).To(BeNil())
 			g.Expect(resp.Kvs).To(HaveLen(1))
+			g.Expect(resp.Header.Revision).ToNot(BeZero())
+			g.Expect(resp.Kvs[0].Key).To(Equal([]byte("/key/1")))
 		})
 	})
 }
@@ -61,8 +104,10 @@ func BenchmarkList(b *testing.B) {
 	client := newKine(b)
 	g := NewWithT(b)
 
-	for i := 0; i < b.N; i++ {
-		key := fmt.Sprintf("key-%d", i)
+	rep := b.N
+
+	for i := 0; i < rep; i++ {
+		key := fmt.Sprintf("key/%d", i)
 		resp, err := client.Txn(ctx).
 			If(clientv3.Compare(clientv3.ModRevision(key), "=", 0)).
 			Then(clientv3.OpPut(key, "benchValue")).
@@ -72,4 +117,15 @@ func BenchmarkList(b *testing.B) {
 		g.Expect(err).To(BeNil())
 		g.Expect(resp.Succeeded).To(BeTrue())
 	}
+
+	b.Run("BenchListCountKeys", func(b *testing.B) {
+		g := NewWithT(b)
+		for i := 0; i < rep; i++ {
+			resp, err := client.Get(ctx, "key/", clientv3.WithPrefix())
+
+			g.Expect(err).To(BeNil())
+			g.Expect(resp.Kvs).To(HaveLen(rep))
+		}
+	})
+
 }
